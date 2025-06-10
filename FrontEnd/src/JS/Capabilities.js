c552bee20d10fccdba69b4eef679a0cfa25727f7
helauren42 <helauren42@gmail.com>
2025-06-10 09:07:23 +0200

    it's a beauty added images to capabilities section all responsive

--- FrontEnd/src/JS/Capabilities.js
+++ FrontEnd/src/JS/Capabilities.js
@@ -1,21 +1,10 @@
 import { useEffect } from 'react'
 import '../CSS/Capabilities.css'
-import blackBagFigthingImg from '../IMAGES/black-bag-fighting.png'
+import { ReactComponent as AugmentedReality } from '../IMAGES/undraw_augmented-reality_3ie0.svg'
+import { ReactComponent as CloudSync } from '../IMAGES/undraw_cloud-sync_h1ig.svg'
+import { ReactComponent as DocumentSearch } from '../IMAGES/undraw_document-search_2o7x.svg'
 
-export const Capabilities = ({ screenWidth }) => {
-  // useEffect(() => {
-  //   const cards = document.getElementsByClassName("capabilities")
-  //   let maxHeight = 0
-  //   for (let i = 0; i < cards.length; i++) {
-  //     cards[i].style.height = 'auto'
-  //     const height = cards[i].clientHeight
-  //     if (maxHeight < height)
-  //       maxHeight = height
-  //   }
-  //   for (let i = 0; i < cards.length; i++) {
-  //     cards[i].style.height = `${maxHeight * 1.12}px`
-  //   }
-  // }, [screenWidth])
+export const Capabilities = () => {
   return (
     <section className='tc-wrapper'>
       <h1 className="trip-card-title">Level up your security with intelligent surveillance and threat detection</h1>
@@ -27,7 +16,8 @@ export const Capabilities = ({ screenWidth }) => {
               <p>Identify suspicious behavior patterns like loitering, group formation, and face-covering before an incident unfolds.
               </p>
             </div>
-            <div className="tc-image">
+            <div className="img-cont">
+              <AugmentedReality className='capability-img' />
             </div>
           </div>
           <div className="classic-trip-card">
@@ -36,18 +26,18 @@ export const Capabilities = ({ screenWidth }) => {
               <p>Trigger smart notifications when critical thresholds are crossed. Events are logged with contextual metadata for instant review and response.
               </p>
             </div>
-            <div className="tc-image">
-              <img />
+            <div className="img-cont">
+              <CloudSync className='capability-img' />
             </div>
           </div>
           <div className="classic-trip-card">
             <div className="capabilities">
               <h2>Searchable Surveillance with AI Summaries</h2>
-              <p>Find key footage using natural language. Instantly retrieve clips that match queries like “Somebody with a black bag fighting”
+              <p>Find key footage using natural language. Instantly retrieve clips that match queries like “Somebody with a black bag fighting.”
               </p>
             </div>
-            <div className="tc-image">
-              <img />
+            <div className="img-cont">
+              <DocumentSearch className='capability-img' />
             </div>
           </div>
         </div>
