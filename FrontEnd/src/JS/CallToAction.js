cdc9fbf7bc525879e1b4a575cf3b7eeabafa2484
helauren42 <helauren42@gmail.com>
2025-06-11 11:28:04 +0200

    cta responsiveness, and improved styling, added text grey color

--- FrontEnd/src/JS/CallToAction.js
+++ FrontEnd/src/JS/CallToAction.js
@@ -5,7 +5,7 @@ export const CallToAction = ({ screenWidth }) => {
   return (
     <section className="page-section" id="call-to-action">
       <div className="short-divider">
-        <p className="section-title" id="cta-title">Schedule a free call now to find how you will benefit from Novatus</p>
+        <p className="minitrip-p" id="cta-title">Schedule a free call now to find how you will benefit from Novatus</p>
         <div className='classic-btn std-hover-active' id="cta-demo">
           <p id="cta-btn">{screenWidth < 450 ? "Demo" : "Request demo"}</p>
           <SendEmailSvg id="send-email-svg" />
